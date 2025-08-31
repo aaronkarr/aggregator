01695905f62eea7becc21a0957b52b964ce32733
Aaron Karr <aaron@aaronkarr.com>
2025-08-30 22:10:48 -0400

    feed aggregation loop

--- handler_agg.go
+++ handler_agg.go
@@ -3,13 +3,52 @@ package main
 import (
 	"context"
 	"fmt"
+	"time"
+
+	"github.com/aaronkarr/aggregator/internal/database"
 )
 
 func handlerAgg(s *state, cmd command) error {
-	feed, err := fetchFeed(context.Background(), "https://www.wagslane.dev/index.xml")
-	if err != nil {
-		return fmt.Errorf("error fetching feed: %w", err)
+	if len(cmd.Args) < 1 || len(cmd.Args) > 2 {
+		return fmt.Errorf("usage: %v <time_between_reqs>", cmd.Name)
 	}
-	fmt.Printf("Feed: %v\n", feed)
-	return nil
+
+	timeBeweenReqs, err := time.ParseDuration(cmd.Args[0])
+	if err != nil {
+		return fmt.Errorf("invalid duration: %w", err)
+	}
+	fmt.Printf("Collecting feeds every %s...\n", timeBeweenReqs)
+
+	ticker := time.NewTicker(timeBeweenReqs)
+
+	for ; ; <-ticker.C {
+		scrapeFeeds(s)
+	}
+}
+
+func scrapeFeeds(s *state) {
+	feed, err := s.db.GetNextFeedToFetch(context.Background())
+	if err != nil {
+		fmt.Printf("couldn't get next feed to fetch: %w", err)
+		return
+	}
+	fmt.Println("Found a feed to fetch!")
+	scrapeFeed(s.db, feed)
+}
+
+func scrapeFeed(db *database.Queries, feed database.Feed) {
+	err := db.MarkFeedFetched(context.Background(), feed.ID)
+	if err != nil {
+		fmt.Printf("couldn't mark feed %s as fetched: %w", feed.Name, err)
+	}
+
+	feedItem, err := fetchFeed(context.Background(), feed.Url)
+	if err != nil {
+		fmt.Printf("error fetching feed %s: %w", feed.Name, err)
+	}
+
+	for _, item := range feedItem.Channel.Item {
+		fmt.Printf("Found post: %s\n", item.Title)
+	}
+	fmt.Printf("Feed %s collected, %v posts found", feed.Name, len(feedItem.Channel.Item))
 }
--- sql/queries/feeds.sql
+++ sql/queries/feeds.sql
@@ -15,3 +15,12 @@ SELECT * FROM feeds;
 
 -- name: GetFeed :one
 SELECT * FROM feeds WHERE url = $1;
+
+-- name: MarkFeedFetched :exec
+UPDATE feeds SET last_fetched_at = updated_at
+WHERE id = $1;
+
+-- name: GetNextFeedToFetch :one
+SELECT * FROM feeds
+ORDER BY last_fetched_at ASC NULLS FIRST
+LIMIT 1;
